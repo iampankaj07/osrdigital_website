0c38a16c1c4c3865db0a6bfe6968c6046d28233a
Iampankaj07 <pankaj.dulal.07@gmail.com>
2025-09-06 12:18:51 +0545

    add team model and mnigration

---
+++ database/migrations/2025_09_06_062616_team.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('team', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('designation')->nullable();
+            $table->string('image')->default('text'); 
+            $table->string('linkedIn')->nullable();
+            $table->string('github')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    Schema::dropIfExists('team');
+    }
+};
